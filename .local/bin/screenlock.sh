fe70904fc1881bfc4ed0fe7398d39b9b1fc94005
junglerobba <tobias.langendorf@googlemail.com>
2019-03-02 18:51:53 +0100

    set better swaylock screen timeout

--- .local/bin/screenlock.sh
+++ .local/bin/screenlock.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # put task in background
 swayidle \
-    timeout 2 'swaymsg "output * dpms off"' \
+    timeout 10 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # lock screen
 swaylock -c000000
 # kill last background task
-# kill %%
 pkill swayidle
