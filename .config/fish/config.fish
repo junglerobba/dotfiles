c89b088bd8f2fd953a2e242446c512d48d23805c
junglerobba <tobias.langendorf@googlemail.com>
2019-11-22 22:53:50 +0100

    fish: add node_modules to path inside project dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,7 @@
 set -gx PATH $HOME/.local/bin $PATH
 set -gx EDITOR vim
+
+# add node_modules to path when shell is launched in project dir
+if test -e ./package.json && test -d ./node_modules/.bin
+	set -x PATH $PATH (pwd)/node_modules/.bin
+end
