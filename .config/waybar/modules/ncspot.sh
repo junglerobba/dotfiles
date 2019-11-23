ef4e663ca155e780839da7148a3d05b0b3628c4d
junglerobba <tobias.langendorf@googlemail.com>
2019-11-23 01:04:02 +0100

    waybar: add ncspot module

---
+++ .config/waybar/modules/ncspot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+class=$(playerctl metadata --player=ncspot --format '{{lc(status)}}')
+icon=""
+
+if [[ $class == "playing" ]]; then
+  info=$(playerctl metadata --player=ncspot --format '{{artist}} - {{title}}')
+  if [[ ${#info} > 60 ]]; then
+    info=$(echo $info | cut -c1-60)"..."
+  fi
+  text=$icon" "$info
+elif [[ $class == "paused" ]]; then
+  text=""
+elif [[ $class == "stopped" ]]; then
+  text=""
+fi
+
+echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
